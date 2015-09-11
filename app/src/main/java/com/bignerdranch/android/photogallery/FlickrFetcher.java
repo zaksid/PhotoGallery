81102b321fc13ba496bb9cb5ae5f759277892cc0
Alexander Mahdybor <thezaksid@gmail.com>
2015-09-11 23:09:55 +0300

    Chapter 26. Challenge: Paging
    Added ability to get photos from all pages and to append them to
    result

--- app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetcher.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetcher.java
@@ -22,6 +22,7 @@ public class FlickrFetcher {
     private static final String API_KEY = "227e5573e03fba075da8990dc6f91c00";
     private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
     private static final String PARAM_EXTRAS = "extras";
+    private static final String PARAM_PAGE = "page";
     private static final String EXTRA_PICTURE_SMALL_URL = "url_s";
     private static final String XML_PHOTO = "photo";
 
@@ -67,13 +68,44 @@ public class FlickrFetcher {
             XmlPullParser parser = factory.newPullParser();
             parser.setInput(new StringReader(xmlRequest));
 
+            int pages = getPagesCount(parser);
             parseItems(items, parser);
+
+            for (int page = 2; page <= pages; page++) {
+                url = Uri.parse(ENDPOINT).buildUpon()
+                        .appendQueryParameter("method", METHOD_GET_RECENT)
+                        .appendQueryParameter("api_key", API_KEY)
+                        .appendQueryParameter(PARAM_EXTRAS, EXTRA_PICTURE_SMALL_URL)
+                        .appendQueryParameter(PARAM_PAGE, String.valueOf(page))
+                        .build().toString();
+                xmlRequest = getUrl(url);
+                Log.i(LOG_TAG, "Received xml: " + xmlRequest);
+                parser.setInput(new StringReader(xmlRequest));
+                parseItems(items, parser);
+            }
+
         } catch (IOException ioe) {
             Log.e(LOG_TAG, "Failed to fetch items", ioe);
         } catch (XmlPullParserException xppe) {
             Log.e(LOG_TAG, "Failed to parse items", xppe);
         }
         return items;
+    }
+
+    private int getPagesCount(XmlPullParser parser)
+            throws XmlPullParserException, IOException {
+        int eventType = parser.next();
+        int pages = 1;
+
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            if (eventType == XmlPullParser.START_TAG && parser.getName().equals("photos")) {
+                pages = Integer.parseInt(parser.getAttributeValue(null, "pages"));
+            }
+
+            eventType = parser.next();
+        }
+
+        return pages;
     }
 
     void parseItems(ArrayList<GalleryItem> items, XmlPullParser parser)
